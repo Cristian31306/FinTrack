0a20057aec1b0db38b09823b9eb73b36087f9d6f
Cristian31306 <est_ca_duran@fesc.edu.co>
2026-03-27 15:16:20 -0500

    debug: add logs to Google Drive auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,8 @@ use Google\Client as GoogleClient;
 use Masbug\Flysystem\GoogleDriveAdapter;
 use League\Flysystem\Filesystem;
 
+use Illuminate\Support\Facades\Log;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -29,19 +31,32 @@ class AppServiceProvider extends ServiceProvider
 
         try {
             Storage::extend('google_drive', function ($app, $config) {
-                $client = new GoogleClient();
-                $client->setClientId($config['clientId']);
-                $client->setClientSecret($config['clientSecret']);
-                $client->setAccessToken($client->fetchAccessTokenWithRefreshToken($config['refreshToken']));
+                try {
+                    $client = new GoogleClient();
+                    $client->setClientId($config['clientId']);
+                    $client->setClientSecret($config['clientSecret']);
+                    
+                    $tokens = $client->fetchAccessTokenWithRefreshToken($config['refreshToken']);
+                    
+                    if (isset($tokens['error'])) {
+                        Log::error('Google Drive Auth Error: ' . ($tokens['error_description'] ?? $tokens['error']));
+                        throw new \Exception('Google Auth Failed: ' . ($tokens['error_description'] ?? $tokens['error']));
+                    }
+                    
+                    $client->setAccessToken($tokens);
 
-                $service = new \Google\Service\Drive($client);
-                $adapter = new GoogleDriveAdapter($service, $config['folderId'] ?? '/');
-                $filesystem = new Filesystem($adapter);
+                    $service = new \Google\Service\Drive($client);
+                    $adapter = new GoogleDriveAdapter($service, $config['folderId'] ?? '/');
+                    $filesystem = new Filesystem($adapter);
 
-                return new \Illuminate\Filesystem\FilesystemAdapter($filesystem, $adapter, $config);
+                    return new \Illuminate\Filesystem\FilesystemAdapter($filesystem, $adapter, $config);
+                } catch (\Exception $e) {
+                    Log::error('In-Storage-Extend Google Drive Error: ' . $e->getMessage());
+                    throw $e;
+                }
             });
         } catch (\Exception $e) {
-            // Silently fail if not configured
+            Log::error('General AppServiceProvider Storage::extend Error: ' . $e->getMessage());
         }
     }
 }
