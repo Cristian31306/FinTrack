c38951ece9d6d771903e7fda3d4ab45feb7aad06
Cristian31306 <est_ca_duran@fesc.edu.co>
2026-03-27 14:44:09 -0500

    fix: register google_drive storage driver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,11 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Storage;
+use Google\Client as GoogleClient;
+use Masbug\Flysystem\GoogleDriveAdapter;
+use League\Flysystem\Filesystem;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,5 +26,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        try {
+            Storage::extend('google_drive', function ($app, $config) {
+                $client = new GoogleClient();
+                $client->setClientId($config['clientId']);
+                $client->setClientSecret($config['clientSecret']);
+                $client->refreshToken($config['refreshToken']);
+
+                $adapter = new GoogleDriveAdapter($client, $config['folderId'] ?? '/');
+                $filesystem = new Filesystem($adapter);
+
+                return new \Illuminate\Filesystem\FilesystemAdapter($filesystem, $adapter, $config);
+            });
+        } catch (\Exception $e) {
+            // Silently fail if not configured
+        }
     }
 }
